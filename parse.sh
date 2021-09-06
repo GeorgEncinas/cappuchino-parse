23bd19c5b33a89a595172cbe528b16f84fe98cb3
Jorge R. Encinas <jorge.encinas@gmail.com>
2021-09-06 14:18:06 -0400

    try manual update pdfs
    - found LICENCIATURA EN BIOLOGIA
    
    - TODO parse from Google Drive link https://docs.google.com/spreadsheets/d/e/2PACX-1vQ18qPask1G-ivHM-yKFHbm4ferQIbcSnhpG8fTOCry_lCC4Y20EQLUichHfNN2eh9ngwDKbIQteG3S/pubhtml?gid=0&amp

--- parse.sh
+++ parse.sh
@@ -26,15 +26,8 @@ cp data/FCyT/news.json public/data/FCyT/.
 cp data/index.json downloads-server/2020.cappuchino/data/.
 cp data/index.json public/data/.
 
-Compress-Archive -Path .\public\* -DestinationPath downloads-server\2020.cappuchino\2021-02.4.cappuchino.zip
+Compress-Archive -Path .\public\* -DestinationPath downloads-server\2020.cappuchino\2021-02.5.cappuchino.zip
 scp -P9195 .\downloads-server\2020.cappuchino\2021-02.4.cappuchino.zip scesi@dozer-ssh.scesi.org:~/.
 
 # sudo unzip 2021-02.4.cappuchino.zip -d /srv/www/2021.cappuchino
 # firebase deploy
-
-# old
-# cp data/FCyT/2020-02/*.json backups/cappuchino-2020-08-31--21-36--backup/data/FCyT/2020-02/.
-# cp data/FCyT/2020-02.json backups/cappuchino-2020-08-31--21-36--backup/data/FCyT/.
-# cp data/FCyT/index.json backups/cappuchino-2020-08-31--21-36--backup/data/FCyT/.
-# cp data/index.json backups/cappuchino-2020-08-31--21-36--backup/data/.
-# cp data/FCyT/news.json backups/cappuchino-2020-08-31--21-36--backup/data/FCyT/.
