31e630119798f081e3f1bae0806dabaecbd80ee1
Jorge R. Encinas <jorge.encinas@gmail.com>
2024-02-27 23:53:33 -0400

    Update parse.sh script for 2024-01 data

--- firebase-deploy.sh
+++ firebase-deploy.sh
@@ -1,7 +1,7 @@
 # firebase section
-mkdir public/data/FCyT/2023-02
-cp data/FCyT/2023-02/*.json public/data/FCyT/2023-02/.
-cp data/FCyT/2023-02.json public/data/FCyT/.
+mkdir public/data/FCyT/2024-01
+cp data/FCyT/2024-01/*.json public/data/FCyT/2024-01/.
+cp data/FCyT/2024-01.json public/data/FCyT/.
 cp data/FCyT/index.json public/data/FCyT/.
 cp data/FCyT/news.json public/data/FCyT/.
 cp data/index.json public/data/.
