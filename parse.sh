26aecb0de4de826eff3d8d56feeb0bf37ad93e97
Jorge R. Encinas <jorge.encinas@gmail.com>
2024-02-27 23:22:43 -0400

    Update parse.sh script for 2024-01 data

--- parse.sh
+++ parse.sh
@@ -1,21 +1,39 @@
-mkdir -p data/FCyT/2023-02
-node get.3.2 2023-02
+mkdir -p data/FCyT/2024-01
+node get2 2024-01
 
 # first add md5 sum for invalid pdf
-node parse2.filter.js 2023-02
+node parse2024.js 409701
+node parse2024.js 320902
+node parse2024.js 299701
+node parse2024.js 650001
+node parse2024.js 429701
+node parse2024.js 166231
+node parse2024.js 760101
+node parse2024.js 359201
+node parse2024.js 309801
+node parse2024.js 134111
+node parse2024.js 114071
+node parse2024.js 439801
+node parse2024.js 349701
+node parse2024.js 319801
+node parse2024.js 339701
+node parse2024.js 389701
+node parse2024.js 411702
 
-node parse2.gestion.js 2023-02 2023-01
+node parse2024.summary.js 2024-01
 
-sudo mkdir -p /srv/www/2022.cappuchino/data/FCyT/2023-02
-sudo cp data/FCyT/2023-02/*.json /srv/www/2022.cappuchino/data/FCyT/2023-02/.
-sudo cp data/FCyT/2023-02.json   /srv/www/2022.cappuchino/data/FCyT/.
+node parse2.gestion.js 2024-01 2023-02
+
+sudo mkdir -p /srv/www/2022.cappuchino/data/FCyT/2024-01
+sudo cp data/FCyT/2024-01/*.json /srv/www/2022.cappuchino/data/FCyT/2024-01/.
+sudo cp data/FCyT/2024-01.json   /srv/www/2022.cappuchino/data/FCyT/.
 sudo cp data/FCyT/index.json     /srv/www/2022.cappuchino/data/FCyT/.
 sudo cp data/FCyT/news.json      /srv/www/2022.cappuchino/data/FCyT/.
 sudo cp data/index.json          /srv/www/2022.cappuchino/data/.
 
-mkdir -p /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/2023-2
-cp data/FCyT/2023-02/*.json /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/2023-2/.
-cp data/FCyT/2023-02.json   /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/.
+mkdir -p /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/2024-1
+cp data/FCyT/2024-01/*.json /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/2024-1/.
+cp data/FCyT/2024-01.json   /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/.
 cp data/FCyT/index.json   /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/index.json
 cp data/FCyT/news.json   /mnt/data/starhuks/api_cappuchino/public/data/departments/fcyt/.
 cd /mnt/data/starhuks/api_cappuchino
