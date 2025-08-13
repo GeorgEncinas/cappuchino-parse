3640a9309bd972ebd85e8b5968b906984ff999b8
Willy Samuel Paz Colque <willypaz243@gmail.com>
2025-08-12 22:09:01 -0400

    chore: update FCyT schedule data and news for August 2025

--- firebase-deploy.sh
+++ firebase-deploy.sh
@@ -1,7 +1,7 @@
 # firebase section
-mkdir public/data/FCyT/2025-01
-cp data/FCyT/2025-01/*.json public/data/FCyT/2025-01/.
-cp data/FCyT/2025-01.json public/data/FCyT/.
+mkdir public/data/FCyT/2025-02
+cp data/FCyT/2025-02/*.json public/data/FCyT/2025-02/.
+cp data/FCyT/2025-02.json public/data/FCyT/.
 cp data/FCyT/index.json public/data/FCyT/.
 cp data/FCyT/news.json public/data/FCyT/.
 cp data/index.json public/data/.
